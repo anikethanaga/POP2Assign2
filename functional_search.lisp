458683f88f766e35e1b3154a51786586b9e908f9
Madhuparna Bhowmik <madhuparnabhowmik04@gmail.com>
2019-04-01 19:42:05 +0530

    Lisp program added

--- functional_search.lisp
+++
@@ -1,6 +0,0 @@
-(defun linear-search (list item)
-  (loop for pos from 0 and element in list
-        when (eql item element)
-          do (return pos)))
- 
-(write (linear-search '(1 3 6 67 56 22 33 39) 6))
